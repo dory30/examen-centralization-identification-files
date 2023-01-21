aee96248de2c2ea216d213abe03372cc49257358
Martine Doriane BILLE <88941170+dory30@users.noreply.github.com>
2023-01-21 02:29:47 +0100

    Add files via upload

---
+++ examen-gateway-server/src/test/java/org/iuc/examen/gateway/ExamenGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iuc.examen.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
