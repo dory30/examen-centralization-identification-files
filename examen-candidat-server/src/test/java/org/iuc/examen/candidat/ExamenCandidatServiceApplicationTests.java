1a8948f56552f974bcccfce28d3c6179ab7c0776
Martine Doriane BILLE <88941170+dory30@users.noreply.github.com>
2023-01-21 11:12:45 +0100

    Add files via upload

---
+++ examen-candidat-server/src/test/java/org/iuc/examen/candidat/ExamenCandidatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iuc.examen.candidat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenCandidatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
