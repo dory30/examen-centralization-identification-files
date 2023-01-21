eedef45e3bf9ea4baa1377350b3c5d4d06e367e7
Martine Doriane BILLE <88941170+dory30@users.noreply.github.com>
2023-01-21 01:45:47 +0100

    Add files via upload

---
+++ examen-identification-server/src/test/java/org/iuc/examen/identification/ExamenIdentificationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iuc.examen.identification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenIdentificationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
